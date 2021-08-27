951295f090a7721a9c57f3ee84e0a0bf964e053e
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-08-27 10:35:15 +0100

    updates

--- database/migrations/1629641619992_users.ts
+++ database/migrations/1629641619992_users.ts
@@ -8,7 +8,7 @@ export default class Users extends BaseSchema {
       table.increments('id')
       table.string('name')
       table.string('email')
-      table.string('usernmae', 25).index()
+      table.string('username', 25).index()
       table.string('avatar').nullable()
       table.text('bio').nullable()
       table.dateTime('email_varified_at').nullable()
