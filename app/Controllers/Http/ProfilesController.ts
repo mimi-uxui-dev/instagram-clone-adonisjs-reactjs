90ff101da65d4ff46627bb1e8d0b38cdd3269fff
mimi-uxui-dev <uxui.mimi@gmail.com>
2021-09-10 08:57:19 +0100

    Creation UserFactory ✔

--- app/Controllers/Http/ProfilesController.ts
+++ app/Controllers/Http/ProfilesController.ts
@@ -12,7 +12,7 @@ export default class ProfilesController {
         }
 
         await user.preload('posts')
-
+        
         return view.render('profile', {user})
     }
 
--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,15 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import User from 'App/Models/User'
+import { DateTime } from 'luxon'
+
+export const UserFactory = Factory.define(User, ({ faker}) => {
+    return {
+        name: faker.name.findName(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        bio: faker.lorem.paragraph(),
+        avatar: faker.image.avatar(),
+        email_varified_at: DateTime.local(),
+    }
+}).build()
